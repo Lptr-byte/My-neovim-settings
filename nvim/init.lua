066c6ed8bffabd98a6312a41a8991026dbdf48d8
= <=>
2023-02-07 00:14:44 +0800

    neovim

---
+++ nvim/init.lua
@@ -0,0 +1,17 @@
+require("plugins.plugins_setup")
+
+----------My Configure------------
+require("core.options")
+require("core.keymaps")
+
+----------Extensions-------------
+require("plugins.lualine")
+require("plugins/nvim-tree")
+require("plugins/treesitter")
+require("plugins/lsp")
+require("plugins/cmp")
+require("plugins/autopairs")
+require("plugins/comment")
+require("plugins/gitsigns")
+require("plugins/bufferline")
+require("plugins/telescope")
